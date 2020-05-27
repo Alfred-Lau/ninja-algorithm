6078de0f6c6bb6aeb83847c137842b1f42f0c4be
潇湘 <liujian@cai-inc.com>
2020-05-28 00:54:25 +0800

    feat: add insertionSort

--- src/sort/insertSort.js
+++ src/sort/insertSort.js
@@ -3,8 +3,31 @@
 空间复杂度：
 
 */
-function insertSort(arr) {}
+
+function less(a, b) {
+  return a < b;
+}
+
+function swap(a, i, j) {
+  const tmp = a[i];
+  a[i] = a[j];
+  a[j] = tmp;
+}
+
+function insertSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i; j > 0; j--) {
+      if (less(arr[j], arr[j - 1])) {
+        swap(arr, j, j - 1);
+      } else {
+        break;
+      }
+    }
+  }
+}
 
 var arr = [6, 5, 4, 3, 2, 1];
+insertSort(arr);
 
-console.log(insertSort(arr));
+console.log(arr);
